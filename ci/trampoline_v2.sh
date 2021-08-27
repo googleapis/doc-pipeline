22bd0b242b392adafa304be18410e8840044122a
Dan Lee <dandhlee@google.com>
2021-08-27 16:38:10 +0000

    test: increase build timeout

--- ci/trampoline_v2.sh
+++ ci/trampoline_v2.sh
@@ -418,7 +418,8 @@ docker_flags=(
     "--env" "TRAMPOLINE_SECRET_DIR=/secrets"
     "--volume" "${KOKORO_GFILE_DIR:-/dev/shm}:/secrets/gfile"
     "--env" "KOKORO_GFILE_DIR=/secrets/gfile"
-    "--volume" "${KOKORO_KEYSTORE_DIR:-/dev/shm}:/secrets/keystore"
+    #"--volume" "${KOKORO_KEYSTORE_DIR:-/dev/shm}:/secrets/keystore"
+    "--volume" "${KOKORO_KEYSTORE_DIR:-/usr/local/google/home/dandhlee/workspace}:/secrets/keystore"
     "--env" "KOKORO_KEYSTORE_DIR=/secrets/keystore"
 )
 
